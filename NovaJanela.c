56d9286a426b36dc5a2946978fe67af829526158
Blako <gustavo.reis@a.ucb.br>
2024-10-29 12:17:16 -0300

    Update NovaJanela.c
    hehe

--- NovaJanela.c
+++ NovaJanela.c
@@ -51,7 +51,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     hwnd = CreateWindowEx(
         WS_EX_CLIENTEDGE,
         g_szClassName,
-        "The title of my window",
+        "PORNOZÃO DO SEIB",
         WS_OVERLAPPEDWINDOW,
         CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
         NULL, NULL, hInstance, NULL);
